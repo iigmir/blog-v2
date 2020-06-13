3d2f5f858f269da88df5fe3da94051ddfe3f379d
iigmir <roc120j@gmail.com>
2020-06-13 23:53:08 +0800

    main function test

--- test/index.spec.js
+++ test/index.spec.js
@@ -5,7 +5,7 @@ const config_path = "test/test-suits/config.json";
 
 describe( "StaticSiteGenerator", () =>
 {
-    it( "should get the input setting by out input", async () =>
+    it( "should get the input setting by input", async () =>
     {
         const app = new StaticSiteGenerator();
         const expected_cases = {
@@ -16,10 +16,10 @@ describe( "StaticSiteGenerator", () =>
         await app.set_config( config_path );
         assert.deepStrictEqual( app.config, expected_cases );
     });
-    it( "should get the directory array by out input", async () =>
+    it( "should get the directory array by input", async () =>
     {
         const app = new StaticSiteGenerator();
-        const expected_cases =  [ "test.spec.md", ];
+        const expected_cases = [ "test.spec.md", ];
         await app.set_config( config_path );
         await app.read_directory();
         assert.deepStrictEqual( app.directory_files, expected_cases );
@@ -31,5 +31,20 @@ describe( "StaticSiteGenerator", () =>
         await app.set_config( config_path );
         await app.read_directory();
         assert.deepStrictEqual( await app.source_markdowns, expected_cases );
+    });
+    describe( "main function", () =>
+    {
+        it( "should works as expected", async () =>
+        {
+            const app = new StaticSiteGenerator();
+            await app.main( config_path );
+            assert.deepStrictEqual( app.config, {
+                source_directory: "test/articles",
+                template_file: "test/test-suits/template.spec.html",
+                destination_directory: "test/docs"
+            } );
+            assert.deepStrictEqual( app.directory_files, [ "test.spec.md", ] );
+            assert.deepStrictEqual( await app.source_markdowns, [ `# Test\n\nHello\n`, ] );
+        });
     });
 });
