4c5ccf885d5092f408054284580615585b4a0222
iigmir <roc120j@gmail.com>
2020-06-13 13:40:57 +0800

    Add mocha test and debug setting

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,1 +1,15 @@
-const main = require( "../src/new-class.js" );
+const StaticSiteGenerator = require( "../src/new-class.js" );
+const assert = require( "assert" );
+
+describe( "StaticSiteGenerator", () =>
+{
+    it( "should get the input setting by out input", () => {
+        const instance = new StaticSiteGenerator( "test/config.json" );
+        const expected = {
+            source_directory: "test/articles",
+            template_file: "test/template.spec.html",
+            destination_directory: "test/docs"
+        };
+        assert.deepStrictEqual( instance.config, expected );
+    });
+});
