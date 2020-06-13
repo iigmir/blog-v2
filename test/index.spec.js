2a6fa6f281204c291dc9103cf8459a9886455e00
iigmir <roc120j@gmail.com>
2020-06-13 16:00:25 +0800

    Move config files

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,22 +1,24 @@
 const StaticSiteGenerator = require( "../src/new-class.js" );
 const assert = require( "assert" );
 
+const config_path = "test/test-suits/config.json";
+
 describe( "StaticSiteGenerator", () =>
 {
     it( "should get the input setting by out input", async () =>
     {
         const instance = new StaticSiteGenerator();
-        await instance.load_config( "test/config.json" );
+        await instance.load_config( config_path );
         assert.deepStrictEqual( instance.config, {
             source_directory: "test/articles",
-            template_file: "test/template.spec.html",
+            template_file: "test/test-suits/template.spec.html",
             destination_directory: "test/docs"
         });
     });
     it( "should get the directory array by out input", async () =>
     {
         const instance = new StaticSiteGenerator();
-        await instance.load_config( "test/config.json" );
+        await instance.load_config( config_path );
         await instance.read_directory();
         assert.deepStrictEqual( instance.directory_files, ["test.spec.md"]);
     });
