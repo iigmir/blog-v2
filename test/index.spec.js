2cfaf7675ac28b1c021c655048ead077ac950e00
iigmir <roc120j@gmail.com>
2020-06-13 14:30:04 +0800

    Rewrite async

--- test/index.spec.js
+++ test/index.spec.js
@@ -3,15 +3,14 @@ const assert = require( "assert" );
 
 describe( "StaticSiteGenerator", () =>
 {
-    it( "should get the input setting by out input", async () => {
+    it( "should get the input setting by out input", async () =>
+    {
         const instance = new StaticSiteGenerator();
-        return instance.load_config( "test/config.json" ).then( result =>
-        {
-            assert.deepStrictEqual( instance.config, {
-                source_directory: "test/articles",
-                template_file: "test/template.spec.html",
-                destination_directory: "test/docs"
-            });
+        await instance.load_config( "test/config.json" );
+        assert.deepStrictEqual( instance.config, {
+            source_directory: "test/articles",
+            template_file: "test/template.spec.html",
+            destination_directory: "test/docs"
         });
     });
 });
