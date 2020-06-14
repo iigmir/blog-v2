8ca963b0382740c9a30c4014e6e51d7ba5f4a7c5
iigmir <roc120j@gmail.com>
2020-06-14 22:53:17 +0800

    AJAX

--- test/ajax-mode-suits/expect-cases.js
+++ test/ajax-mode-suits/expect-cases.js
@@ -5,22 +5,7 @@ const directory_files = [
     "https://raw.githubusercontent.com/iigmir/blog-source/master/README.md"
 ];
 const source_markdowns = [
-    `# Blog source
-Articles from my old blog. May be my new blog site?
-
-## Scripts
-### How to use
-Except shell script \`./scripts/generate.sh\` should use following command:
-\`\`\`
-$ chmod 777 ./scripts/generate.sh
-$ ./scripts/generate.sh
-\`\`\`
-Other scripts are written in JavaScript files, should use following command:
-\`\`\`
-$ node ./scripts/***.sh
-\`\`\`
-
-About what those scripts for, refer each script files.`
+    "# Blog source\nArticles from my old blog. May be my new blog site?\n\n## Scripts\n### How to use\nExcept shell script `./scripts/generate.sh` should use following command:\n```\n$ chmod 777 ./scripts/generate.sh\n$ ./scripts/generate.sh\n```\nOther scripts are written in JavaScript files, should use following command:\n```\n$ node ./scripts/***.sh\n```\n\nAbout what those scripts for, refer each script files."
 ];
 const template = read_file( "test/ajax-mode-suits/template.spec.html" );
 const parsed_htmls = read_array_file([ read_file( "test/ajax-mode-suits/expected.html" ) ]);
