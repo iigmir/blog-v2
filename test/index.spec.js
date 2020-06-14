1ee9851b5dcaa01df566bf1b699cf2c047e7cf42
iigmir <roc120j@gmail.com>
2020-06-14 15:34:41 +0800

    Main function working

--- test/index.spec.js
+++ test/index.spec.js
@@ -41,29 +41,12 @@ describe( "StaticSiteGenerator", () =>
         await app.set_template();
         assert.deepStrictEqual( await app.parsed_htmls, await expected.parsed_htmls );
     } );
-    it( "should write parsed HTMLs to specified destination directory", async() =>
-    {
-        const app = new StaticSiteGenerator();
-        await app.set_config( config_path );
-        await app.read_directory();
-        await app.set_template();
-        assert.doesNotThrow( async() => await app.write_files(), Error );
-    } );
-    /*
     describe( "main function", () =>
     {
-        it( "should works as expected", async () =>
+        it( "should works as expected", async() =>
         {
             const app = new StaticSiteGenerator();
-            await app.main( config_path );
-            assert.deepStrictEqual( app.config, {
-                source_directory: "test/articles",
-                template_file: "test/test-suits/template.spec.html",
-                destination_directory: "test/docs"
-            } );
-            assert.deepStrictEqual( app.directory_files, [ "test.spec.md", ] );
-            assert.deepStrictEqual( await app.source_markdowns, [ `# Test\n\nHello\n`, ] );
-        });
-    });
-    */
+            assert.doesNotThrow( async() => await app.main( config_path ), Error );
+        } );
+    } );
 } );
