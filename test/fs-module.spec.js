2cf5424c78f7200ac55e212106a9fabe1c162e4a
iigmir <roc120j@gmail.com>
2020-06-26 20:24:27 +0800

    Add FileSystemModule test

--- test/fs-module.spec.js
+++ test/fs-module.spec.js
@@ -3,10 +3,29 @@ const FileSystemModule = require( "../lib/FileSystemModule.js" );
 
 describe( "FileSystemModule", () =>
 {
-    it( "should read the file", async() =>
+    it( "should read a file", async() =>
     {
         const app = new FileSystemModule();
-        let text = await app.read_file( "test/fs-module-suits/read-file.txt" );
-        assert.deepEqual( text.trim(), "Hello World!" );
+        let test = await app.read_file( "test/fs-module-suits/read-file.txt" );
+        assert.deepEqual( test.trim(), "Hello World!" );
+    });
+    it( "should read a directory", async() =>
+    {
+        const app = new FileSystemModule();
+        let test = await app.read_directory( "test/fs-module-suits" );
+        assert.deepEqual( test.length, 3 );
+    });
+    it( "should read the JSON file if it's vaild", async() =>
+    {
+        const app = new FileSystemModule();
+        let test = await app.read_json( "test/fs-module-suits/vaild.json" );
+        assert.deepEqual( test.api, 123456 );
+    });
+    it( "should throw error read_json reads invaild JSON file", async() =>
+    {   // https://stackoverflow.com/questions/52449922
+        const app = new FileSystemModule();
+        await assert.rejects((
+            async() => await app.read_json( "test/fs-module-suits/read-file.txt" )
+        )());
     });
 });
