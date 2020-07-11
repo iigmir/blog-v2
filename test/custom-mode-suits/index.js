047e8066604070a657e55e986ebb3b5b5df31543
iigmir <roc120j@gmail.com>
2020-07-12 06:33:26 +0800

    Delete all testing files, rewrite later.

--- test/custom-mode-suits/index.js
+++
@@ -1,9 +0,0 @@
-const FileSystemModule = require( "../../lib/FileSystemModule.js" );
-const fsm = new FileSystemModule();
-
-fsm.write_file({
-    path: "test/custom-mode-suits/docs/test.txt",
-    data: "Hello World",
-});
-
-// console.log( "done" );
