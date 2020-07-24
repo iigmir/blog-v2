ce4d7a2fc8ed9e3ba82aec24a77196986371b864
iigmir <roc120j@gmail.com>
2020-07-25 01:12:28 +0800

    Add tags

--- docs/tags/index.js
+++ docs/tags/index.js
@@ -3,13 +3,31 @@ $( document ).ready(() =>
 {
     const init_tags = async() =>
     {
-        const articles = await $.ajax({
+        let articles = await $.ajax({
             url: "https://raw.githubusercontent.com/iigmir/blog-source/master/info-files/new-category-ids.json"
         });
-        const tags = await $.ajax({
+        let tags = await $.ajax({
             url: "https://raw.githubusercontent.com/iigmir/blog-source/master/info-files/categories.json"
         });
-        return { articles, tags };
+        try
+        {
+            return {
+                articles: JSON.parse( articles ),
+                tags: JSON.parse( tags ),
+            };
+        }
+        catch( error )
+        {
+            return { articles, tags };
+        }
     };
-    const datas = init_tags();
+    init_tags().then( data =>
+    {
+        const tag_template = ( input_tag = { tag_name: "", id: 0 }) =>
+            `<a href="javascript: void(0)" class="button" data-i-btn-id="${ input_tag.id }">
+                ${ input_tag.tag_name }
+            </a>`
+        ;
+        const tag_templates = 
+    });
 });
