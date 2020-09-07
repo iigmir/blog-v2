1d8439634d819d911cf4eb6bd4d45738b0f54e2a
iigmir <roc120j@gmail.com>
2020-09-07 20:08:26 +0800

    Improve page

--- src/new-index-text.js
+++ src/new-index-text.js
@@ -1,3 +1,15 @@
-module.exports = ( url = "index.html", title = "Foobar" ) => (
-    `<a href="${ url }">${ title }</a>`
+const render_list_items = ( url = "index.html", title = "Foobar" ) => (
+    `<li><a href="${ url }">${ title }</a></li>\n`
 );
+
+const main = ( array = []) => (
+    `<ul>
+        ${
+    array
+        .map( item => render_list_items( item.id + ".html", item.title ))
+        .join( "" )
+    }
+    </ul>`
+);
+
+module.exports = main;
