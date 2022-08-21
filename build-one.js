a64efdd7ecc39219b0f1b7e3f07c917521196523
iigmir <roc120j@gmail.com>
2022-08-21 11:36:43 +0800

    Build one file script added

---
+++ build-one.js
@@ -0,0 +1,18 @@
+const BlogAJAXGenerator = require( "./lib/BlogAJAXGenerator" );
+const main = (id = "ERROR") =>
+{
+    const app = new BlogAJAXGenerator();
+    const name = `${ id }.md`;
+    app.main({
+        "mode": "ajax",
+        "source_directory": [ {
+            name,
+            download_url: `https://raw.githubusercontent.com/iigmir/blog-source/master/articles/${ id }.md`,
+        } ],
+        "template_file": "src/template.html",
+        "destination_directory": "docs/articles",
+        "replaced_text": "<the-article />"
+    });
+};
+
+main( process.argv[2] );
