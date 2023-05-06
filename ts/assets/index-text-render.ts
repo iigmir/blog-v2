df07cf951f63d8d058278e76c1e17731ab259c47
iigmir <roc120j@gmail.com>
2023-05-06 23:47:07 +0800

    Render text list

--- ts/assets/index-text-render.ts
+++ ts/assets/index-text-render.ts
@@ -6,7 +6,9 @@ const get_name = (id: number) => String(id).padStart(3, "0");
 
 const action = (item: BlogArticleInfoInterface): string => render_list_items(`${get_name(item.id)}.html`, item.title);
 
-const main = (array: BlogArticleInfoInterface[]) => (`<ul>${array.map(action).join("")}</ul>`);
+const main = (array: BlogArticleInfoInterface[]) => (`<ul>
+${array.map(action).join("")}
+</ul>`);
 
 export default main;
 
