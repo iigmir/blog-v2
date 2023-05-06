8a5b0261a021f142b46ac6e355ffb6f063f66d0b
iigmir <roc120j@gmail.com>
2023-05-06 21:52:27 +0800

    WTFM

--- ts/types/index.ts
+++ ts/types/index.ts
@@ -1,17 +1,43 @@
 export enum ConfigModeEnum {
+    /**
+     * `ts/generators/local-files`
+     */
     Local = "local",
+    /**
+     * `ts/generators/ajax`
+     */
     Ajax = "ajax",
     AjaxIndex = "ajax-index",
+    /**
+     * `ts/generators/template`
+     */
     Template = "template",
+    /**
+     * Unknown generator.
+     */
     Unknown = "unknown",
-    // Unknown = "",
 }
 
 export interface ConfigInterface {
+    /**
+     * Generator mode. The property specifies which generator in `ts/generators` should use.
+     */
     mode: ConfigModeEnum,
+    /**
+     * A source API for a HTTP request in the generator.
+     */
     source_directory: string,
+    /**
+     * Template file. The property specifies which template file in `ts/assets` should render.
+     */
     template_file: string,
+    /**
+     * Output path. Currently the property starts with `docs/`.
+     */
     destination_directory: string,
+    /**
+     * A keyword for the content to replace with.
+     */
     replaced_text: string,
 }
 
