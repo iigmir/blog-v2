998d0882189786241255432538bcf4a57995f841
iigmir <roc120j@gmail.com>
2023-05-06 14:31:28 +0800

    Fix read_source_markdowns

--- ts/utils/ajax.ts
+++ ts/utils/ajax.ts
@@ -1,4 +1,6 @@
 import axios from "axios";
+import type { AxiosResponse } from "axios";
+// AxiosResponse
 
 export const ajax_url = (source_directory: string) => axios(source_directory);
 
@@ -23,10 +25,17 @@ export const read_source_directory = ( source_directory: string ) =>
  */
 export const read_source_markdowns = async(directory_files: string[]) =>
 {
-    // : Promise<string[]>
     return new Promise( (resolve, reject) => {
-        const fn = directory_files.map( url => axios.get( url ) );
-        Promise.all( fn ).then( results => resolve(results) ).catch( error => reject( error ) );
+        const apis = directory_files.map( url => axios.get( url ) );
+        /**
+         * `Promise.all` will return a group of arrays, we therefore need to get their content, aka `data`.
+         * The type of `data` itself is a Markdown string.
+         * @param results 
+         * @returns 
+         */
+        const resolve_action = (results: AxiosResponse[]) => resolve( results.map( its => its.data as string ) );
+        const reject_action: ((reason: any) => void | PromiseLike<void>) | null | undefined = error => reject(error);
+        Promise.all( apis ).then( resolve_action ).catch( reject_action );
     });
 }
 
