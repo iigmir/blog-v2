1c56897740915fd552c83b98231e9b76b4eac3ec
iigmir <roc120j@gmail.com>
2023-10-30 22:12:43 +0800

    Init methods

--- docs/assets/article.js
+++ docs/assets/article.js
@@ -1,13 +1,32 @@
 class ArticleTagsApp {
+    constructor() {}
+    // ID module
     id = ""
-    constructor() {
-        this.set_id();
-        console.log( this.id );
-    }
     set_id() {
         const current_article_path = location.pathname.split("articles/")[1];
         if( current_article_path ) {
             this.id = current_article_path.split(".")[0];
+        }
+    }
+    // AJAX module
+    get api_path() {
+        const domain = "https://iigmir.serv00.net";
+        const api = "/api/blog-metadata.php";
+        const param = `?id=${this.id}`;
+        return domain + api + param;
+    }
+    request_api() {
+        console.log( this.api_path );
+    }
+    // Render module
+    render_element() {
+        // 
+    }
+    main() {
+        this.set_id();
+        if( this.id ) {
+            this.request_api();
+            this.render_element();
         }
     }
 }
@@ -17,6 +36,7 @@ class ArticleTagsAppELement extends HTMLElement {
     constructor() {
         super();
         this.tags_object = new ArticleTagsApp();
+        this.tags_object.main();
     }
 }
 
