0c5ece6a37cf777a2bdb4805459dc99533f57f08
iigmir <roc120j@gmail.com>
2023-10-30 22:02:20 +0800

    Do article tags app

--- docs/assets/article.js
+++ docs/assets/article.js
@@ -1,5 +1,22 @@
+class ArticleTagsAppELement extends HTMLElement {
+    id = ""
+    constructor() {
+        super();
+        this.set_id();
+        console.log( this.id );
+    }
+    set_id() {
+        const current_article_path = location.pathname.split("articles/")[1];
+        if( current_article_path ) {
+            this.id = current_article_path.split(".")[0];
+        }
+    }
+    attributeChangedCallback(name, oldValue, newValue) {
+    }
+}
+
 function main() {
-    console.log("Blog: This is an article.");
+    customElements.define("article-tags-app", ArticleTagsAppELement);
 }
 
 main();
