6a26966af874d4e8b566a8d9db9d7afb5dc5e24a
iigmir <roc120j@gmail.com>
2024-02-24 21:58:22 +0800

    Refactor write_files_to_destination

--- src/ts/utils/fs.ts
+++ src/ts/utils/fs.ts
@@ -1,37 +1,5 @@
 import { readFile, readdir, writeFile } from "fs";
 
-export const file_can_parse = (destination_directory: string, directory_files: string[], parsed_htmls: string[]) =>
-{
-    const result = [
-        Array.isArray( directory_files ),
-        destination_directory,
-        directory_files.length > 0,
-        parsed_htmls.length > 0,
-    ].some( condition => condition === false );
-    if( result === true )
-    {
-        console.warn({
-            message: "The app cannot parse files because certain files failed to meet conditions.",
-            source: { destination_directory, directory_files, parsed_htmls },
-            result,
-        });
-    }
-    return result === false;
-}
-
-export const md_file_name = ( md_name = "", mode = "local" ) =>
-{
-    const altered_html_name = md_name.replace( /.md$/g, ".html" );
-    switch ( mode )
-    {
-        case "local": return altered_html_name;
-        case "ajax":
-            const s = altered_html_name.match( /[^/]+(?=$)/g );
-            return s ? s[0] : "";
-        default: return md_name;
-    }
-}
-
 export const write_file = ({ path = "", data = "" }) =>
 {
     writeFile( path, data, "utf8", ( err ) => 
@@ -42,6 +10,36 @@ export const write_file = ({ path = "", data = "" }) =>
 
 export const write_files_to_destination = ({ dest_dir = "", dir_files = [""], parsed_htmls = [""], mode = "local" }) =>
 {
+    const file_can_parse = (destination_directory: string, directory_files: string[], parsed_htmls: string[]) =>
+    {
+        const result = [
+            Array.isArray( directory_files ),
+            destination_directory,
+            directory_files.length > 0,
+            parsed_htmls.length > 0,
+        ].some( condition => condition === false );
+        if( result === true )
+        {
+            console.warn({
+                message: "The app cannot parse files because certain files failed to meet conditions.",
+                source: { destination_directory, directory_files, parsed_htmls },
+                result,
+            });
+        }
+        return result === false;
+    };
+    const md_file_name = ( md_name = "", mode = "local" ) =>
+    {
+        const altered_html_name = md_name.replace( /.md$/g, ".html" );
+        switch ( mode )
+        {
+            case "local": return altered_html_name;
+            case "ajax":
+                const s = altered_html_name.match( /[^/]+(?=$)/g );
+                return s ? s[0] : "";
+            default: return md_name;
+        }
+    };
     const passed = file_can_parse(dest_dir, dir_files, parsed_htmls);
     if( passed )
     {
@@ -90,7 +88,9 @@ export const read_json = ( file_path = "" ) =>
     {
         read_file(file_path).then( content =>
         {
-            if( get_json(content as string) ) resolve(get_json(content as string));
+            if( get_json(content as string) ) {
+                resolve(get_json(content as string));
+            }
             reject( content );
         }).catch( e => reject(e) );
     });
