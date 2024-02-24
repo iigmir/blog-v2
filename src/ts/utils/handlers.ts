0bb20b75fca0023cc1c8542d70a9a88d7ee8265c
iigmir <roc120j@gmail.com>
2024-02-24 21:41:54 +0800

    fs_write_a_file_to_destination

--- src/ts/utils/handlers.ts
+++ src/ts/utils/handlers.ts
@@ -1,10 +1,4 @@
-import FileSystemModule from "./FileSystemModule";
 import { read_directory } from "./fs";
-
-export const fs_write_a_file_to_destination = ({ path = "", data = "", }) =>
-{
-    return FileSystemModule.write_file({ path, data });
-}
 
 export const fs_read_source_directory = ( source_directory = "" ) =>
 {
