2b9ad01b44be286592c7b00aa0abf16b8b1d9f2b
iigmir <roc120j@gmail.com>
2024-04-16 22:03:20 +0800

    render articles

--- docs/landing.js
+++ docs/landing.js
@@ -31,7 +31,7 @@ class ArticlePreviewItem extends HTMLElement {
         const tags_api = get_tags_api(this.getAttribute("tags-api"));
         const dom = template.querySelector("*[data-app='tags']");
         tags.forEach( (tag) => {
-            const item = tags_api.find( (its) => its.id === tag );
+            const item = tags_api.find( (its) => its.id === tag ) ?? { "id": 0, "tag_name": "unknown" };
             const span = document.createElement("span");
             span.classList.add("item");
             span.dataset.app = "tag-id";
@@ -39,21 +39,32 @@ class ArticlePreviewItem extends HTMLElement {
             span.textContent = item.tag_name;
             dom.appendChild(span);
         });
-        // <span class="item" data-app="tag-id" data-i-btn-id={TAG-ID}>{TAG-TITLE}</span>
+    }
+    set_lang(template) {
+        template.lang = this.getAttribute("language");
     }
     constructor() {
         super();
         const template = document.getElementById("article-preview-item").content;
+        // this.observedAttributes = [
+        //     "tags",
+        //     "tags-api",
+        // ];
 
         // Append DOMs
         this.set_link(template);
         this.set_date(template);
         this.set_tags(template);
+        this.set_lang(template);
 
         // Append shadow root
         const shadowRoot = this.attachShadow({ mode: "open" });
         shadowRoot.appendChild(template.cloneNode(true));
     }
+    // attributeChangedCallback(name, oldValue, newValue) {
+    //     console.log(`Attribute ${name} changed from ${oldValue} to ${newValue}`);
+    //     // You can perform further actions here based on the attribute change
+    // }
 };
 customElements.define("article-preview-item", ArticlePreviewItem);
 
@@ -71,8 +82,29 @@ const get_article_apis = () => {
 // Rendering
 const defa = [{ "id": 0, "title": "", "category_id": [0], "language": "en", "created_at": "1970-01-01T00:00:00Z", "updated_at": "1970-01-01T00:00:00Z" }];
 const deft = [{ "id": 0, "tag_name": "unknown" }];
+/**
+ * <article-preview-item
+    aid="0"
+    title="Hello"
+    tags="[0]"
+    tags-api="[{'id':0,'tag_name':'unknown'}]"
+    language="en"
+    created="1970-01-01T00:00:00Z"
+></article-preview-item>
+ * @param {*} latest_articles 
+ * @param {*} tags 
+ */
 const render_articles = (latest_articles = defa, tags = deft) => {
-    // console.log(latest_articles, tags);
+    latest_articles.forEach( (its) => {
+        const dom = document.createElement("article-preview-item");
+        dom.setAttribute("aid", its.id);
+        dom.setAttribute("title", its.title);
+        dom.setAttribute("tags", JSON.stringify(its.category_id));
+        dom.setAttribute("tags-api", JSON.stringify(tags));
+        dom.setAttribute("language", its.language);
+        dom.setAttribute("created", its.created_at);
+        document.querySelector("#loading-appapp").appendChild( dom );
+    });
 };
 
 // Actions
